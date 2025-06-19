9a30be234c3dda761e7ba394dc4e614e752fea9c
hoangsfr <hvugaconz@gmail.com>
2025-06-19 03:40:23 +0000

    hoangsfr

--- springBoot14/complete/src/main/java/com/example/servingwebcontent/PhieuMuonUtils.java
+++ springBoot14/complete/src/main/java/com/example/servingwebcontent/PhieuMuonUtils.java
@@ -1,3 +1,5 @@
+package com.example.servingwebcontent;
+
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
@@ -5,9 +7,7 @@ public class PhieuMuonUtils {
     public static boolean kiemTraSachGanDenHanTra(PhieuMuon phieuMuon, int soNgayCanhBao) {
         LocalDate ngayHienTai = LocalDate.now();
         LocalDate ngayHetHan = phieuMuon.getNgayHetHan();
-
         long khoangCach = ChronoUnit.DAYS.between(ngayHienTai, ngayHetHan);
-
         return khoangCach > 0 && khoangCach <= soNgayCanhBao;
     }
 }
